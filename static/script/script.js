7ecd4c4498a728dae497b69c8039e88869b05629
Vinicin1101 <vserafim_o@outlook.com>
2022-09-12 21:38:50 -0300

    ahhhh

--- static/script/script.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.querySelector('#check');
-const playlist = document.querySelector('#playlist-content');
-
-// Verificar a checkbox e mostrar ou esconder a playlist
-checkbox.addEventListener('change', function () {
-  
-
-});
-
