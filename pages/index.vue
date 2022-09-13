342b76af0fb078082af3dae80ca7f859154f5e37
Vinicin1101 <vserafim_o@outlook.com>
2022-09-12 21:15:06 -0300

    main commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="body">
+    <ProfileHeader></ProfileHeader>
+
+    <i class="bx bxs-hard-hat"></i>
+    Em breve...
+  </section>
+</template>
+
+<script>
+import ProfileHeader from '~/components/ProfileHeader.vue'
+export default {
+  name: 'IndexPage',
+  components: { ProfileHeader },
+}
+</script>
---
+++ static/script/script.js
@@ -0,0 +1,9 @@
+const checkbox = document.querySelector('#check');
+const playlist = document.querySelector('#playlist-content');
+
+// Verificar a checkbox e mostrar ou esconder a playlist
+checkbox.addEventListener('change', function () {
+  
+
+});
+
